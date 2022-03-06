cee485fb0ba75a9aa2ec4429ffcc7dbf747bdf67
CaffeinePain <whwowns1021@gmail.com>
2022-03-06 21:20:03 +0900

    globalization

--- my-bot/deploy-commands.js
+++ my-bot/deploy-commands.js
@@ -18,25 +18,25 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-    guildIds.map(async(guildId) => {
-        try {
-        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-            body: commands,
-        });
-        console.log(`${guildId} 서버 성공`);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-    //이거 글로벌 커맨드 등록하는 놈인데 버그 있으니까 쓰지 말자
-    // try {
-    //     await rest.put(Routes.applicationCommands(clientId), {
+    // guildIds.map(async(guildId) => {
+    //     try {
+    //     await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
     //         body: commands,
     //     });
-    //     console.log(`글로벌 명령어 등록 성공`);
-    // }
-    // catch (error) {
-    //     console.error(error);
-    // }
+    //     console.log(`${guildId} 서버 성공`);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // });
+
+    //이거 글로벌 커맨드 등록하는 놈인데 버그 있으니까 쓰지 말자
+    try {
+        await rest.put(Routes.applicationCommands(clientId), {
+            body: commands,
+        });
+        console.log(`글로벌 명령어 등록 성공`);
+    }
+    catch (error) {
+        console.error(error);
+    }
 })();
