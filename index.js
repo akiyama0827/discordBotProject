c966a727d470a89fc409e90f94fed3de8be09afc
CaffeinePain <whwowns1021@gmail.com>
2022-03-15 22:35:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Client, Collection, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const { token } = process.env.token;
+console.log(token);
 const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
